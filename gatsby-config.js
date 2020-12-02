ef917bebba27cf5c45ee2636abc3f844fc2eb992
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2020-12-01 21:50:46 -0600

    added boilerplate with storefront config

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,15 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: `gatsby-source-shopify`,
+      options: {
+        // The domain name of your Shopify shop.
+        shopName: `rosatech-solutions.myshopify.com`,
+        // The storefront access token
+        accessToken: `796ec26354eeac6b64eb57af1b95ba03`,
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
